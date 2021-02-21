72095d14b92d7ad8559ef0d914af9a1928e36c64
kaustubhdeokarsudo <receiptsdeokar@gmail.com>
2021-02-21 12:14:45 +0530

    add first GET router

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 const express = require('express')
 const app = express()
 
+app.get('/users',function(req,res){
+    res.json({
+        success:true,
+        message:'successfully got users, nice!',
+        users:[{name:'mark'},{name:'jill'}]
+    })
+})
+
+
 app.listen(8000,function(){
     console.log("Server is listening");
 })
