3e7dcbc6da110e8b70f8bb7337750d6fceb24afc
kaustubhdeokarsudo <receiptsdeokar@gmail.com>
2021-02-21 12:17:15 +0530

    add dynamic GET route

--- server.js
+++ server.js
@@ -9,6 +9,14 @@ app.get('/users',function(req,res){
     })
 })
 
+app.get('/users/:id',function(req,res){
+    console.log(req.params.id);
+    res.json({
+        success:true,
+        message:'successfully got users, nice!',
+        user:req.params.id
+    })
+})
 
 app.listen(8000,function(){
     console.log("Server is listening");
